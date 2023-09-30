70eae57aef3e5d88c2bc233c840c94f01efd4712
wilgar <wilenri341@hotmail.com>
2023-09-30 12:47:07 -0500

    Se Agrego el estado local para mostrar la vista de crear cuenta o de hacer log in.

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { ShoppingCartContext } from "../../Context";
 import Layout from '../../Components/Layout';
@@ -8,45 +8,61 @@ function SignIn() {
 
   const context = useContext(ShoppingCartContext);
 
-   // Account
-   const account = localStorage.getItem("account");
+  const [view, setView] =useState("user-info");
 
-   const parsedAccount = JSON.parse(account);
+  // Account
+  const account = localStorage.getItem("account");
+
+  const parsedAccount = JSON.parse(account);
  
-   // Has an account
-   const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
-   const noAccountInLocalStage = context.account ? Object.keys(context.account).length === 0 : true;
-   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalStage;
+  // Has an account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
+  const noAccountInLocalStage = context.account ? Object.keys(context.account).length === 0 : true;
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalStage;
+
+  const renderLogIn = () => {
+
+    return (
+      <div className='flex flex-col w-80'>
+        <p>
+          <span className='font-light text-sm'>Email: </span>
+          <span>{parsedAccount?.email}</span>
+        </p>
+        <p>
+          <span className='font-light text-sm'>Password: </span>
+          <span>{parsedAccount?.password}</span>
+        </p>
+        <Link
+          to="/">
+          <button
+            className='bg-violet-600 disabled:bg-black/40 text-white w-full py-3 mt-4 mb-2 font-bold'
+            disabled={!hasUserAnAccount}> 
+              Log in
+          </button>
+          </Link>
+        <div className='text-center'> 
+          <a className='font-light text-xs underline underline-offset-3' href='/'>Forgot my password</a>
+        </div>
+          <button
+            className='border  border-violet-600 disabled:text-black/40  disabled:border-violet/40 py-3 mt-6 font-bold'
+            disabled={hasUserAnAccount}
+            onClick={() => setView("create-user-info")}>
+              Sign up
+          </button>
+      </div>
+    )
+  }
+
+  const renderCreateUserInfo = () => {
+  
+  }
+  
+  const renderView = () => view === "create-user-info" ? renderCreateUserInfo() : renderLogIn()
   
     return (
       <Layout>
         <h1 className='font-medium text-lg text-center mb-8 w-80'>Welcome</h1>
-        <div className='flex flex-col w-80'>
-          <p>
-            <span className='font-light text-sm'>Email: </span>
-            <span>{parsedAccount?.email}</span>
-          </p>
-          <p>
-            <span className='font-light text-sm'>Password: </span>
-            <span>{parsedAccount?.password}</span>
-          </p>
-          <Link
-            to="/">
-            <button
-              className='bg-violet-600 disabled:bg-black/40 text-white w-full py-3 mt-4 mb-2 font-bold'
-              disabled={!hasUserAnAccount}> 
-                Log in
-            </button>
-          </Link>
-          <div className='text-center'> 
-            <a className='font-light text-xs underline underline-offset-3' href='/'>Forgot my password</a>
-          </div>
-          <button
-              className='border  border-violet-600 disabled:text-black/40  disabled:border-violet/40 py-3 mt-6 font-bold'
-              disabled={hasUserAnAccount}>
-               Sign up
-          </button>
-        </div>
+        {renderView()}
       </Layout>
     )
   }
