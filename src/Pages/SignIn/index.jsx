5f463ae68fefb61f6ed707f32527688742a738c5
wilgar <wilenri341@hotmail.com>
2023-09-30 12:26:12 -0500

    Se agregó funcionalidad para saber si el usuario tiene cuenta o no para mostrar los datos y habilitar/deshabilitar botones.

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,8 +1,22 @@
-import { Link } from 'react-router-dom'
-import Layout from '../../Components/Layout'
+import { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import { ShoppingCartContext } from "../../Context";
+import Layout from '../../Components/Layout';
 
 
 function SignIn() {
+
+  const context = useContext(ShoppingCartContext);
+
+   // Account
+   const account = localStorage.getItem("account");
+
+   const parsedAccount = JSON.parse(account);
+ 
+   // Has an account
+   const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
+   const noAccountInLocalStage = context.account ? Object.keys(context.account).length === 0 : true;
+   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalStage;
   
     return (
       <Layout>
@@ -10,16 +24,17 @@ function SignIn() {
         <div className='flex flex-col w-80'>
           <p>
             <span className='font-light text-sm'>Email: </span>
-            <span>satoshi.nakamoto@email.com</span>
+            <span>{parsedAccount?.email}</span>
           </p>
           <p>
             <span className='font-light text-sm'>Password: </span>
-            <span>*******</span>
+            <span>{parsedAccount?.password}</span>
           </p>
           <Link
             to="/">
             <button
-              className='bg-violet-600 disabled:bg-black/40 text-white w-full py-3 mt-4 mb-2 font-bold'>
+              className='bg-violet-600 disabled:bg-black/40 text-white w-full py-3 mt-4 mb-2 font-bold'
+              disabled={!hasUserAnAccount}> 
                 Log in
             </button>
           </Link>
@@ -27,7 +42,8 @@ function SignIn() {
             <a className='font-light text-xs underline underline-offset-3' href='/'>Forgot my password</a>
           </div>
           <button
-              className='border  border-violet-600 disabled:text-black/40  disabled:border-black/40 py-3 mt-6 font-bold'>
+              className='border  border-violet-600 disabled:text-black/40  disabled:border-violet/40 py-3 mt-6 font-bold'
+              disabled={hasUserAnAccount}>
                Sign up
           </button>
         </div>
